df62c04c28c61aa4ad4d83b2903f16d3aeee4103
Taoufik El Aoumari <taoufix@gmail.com>
2015-10-05 10:27:35 +0200

    Fix: preven cross-domain injection.

--- userscripts/check-password-field.user.js
+++ userscripts/check-password-field.user.js
@@ -2,18 +2,26 @@
 // @name        Check passoword field is safe
 // @namespace   htt://taoufix.com/password
 // @include     *
-// @version     1.1.0
+// @version     1.2.0
 // @grant       none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
 
+var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
+
+var l = getLocation("http://example.com/path");
+
 $('input[type=password]').each(function () {
     var ok;
     var action = $(this).closest('form').attr('action');
-    if (action.startsWith('https://')) {
-        // Form action is HTTPS :)
+    if (action.startsWith('https://') && getLocation(action).hostname === location.hostname) {
+        // Form action is HTTPS and is in the same domain :)
         ok = true;
     } else if (action.startsWith('http://')) {
         // Form action is HTTP :(
