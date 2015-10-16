cfccf00f5be2bca082f11b4883dc41b4d34a2b2a
Taoufik El Aoumari <taoufix@gmail.com>
2015-10-16 11:37:40 +0200

    Override background images.

--- userscripts/check-password-field.user.js
+++ userscripts/check-password-field.user.js
@@ -2,12 +2,19 @@
 // @name        Check passoword field is safe
 // @namespace   htt://taoufix.com/password
 // @include     *
-// @version     1.2.1
+// @version     1.2.3
 // @grant       none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
+
+var pwStyle = "input.pwOK { background-color: #ccffcc !important; background-image: none !important; }"
+    + "input.pwWarn { background-color: #ffff98 !important; background-image: none !important; }"
+    + "input.pwKO { background-color: #ffbbbb !important; background-image: none !important; }";
+
+
+$( "<style>" + pwStyle + "</style>" ).appendTo("head");
 
 var getLocation = function(href) {
     var l = document.createElement("a");
@@ -15,25 +22,33 @@ var getLocation = function(href) {
     return l;
 };
 
-var l = getLocation("http://example.com/path");
-
 $('input[type=password]').each(function () {
     var ok;
     var action = $(this).closest('form').attr('action');
-    if (action.startsWith('https://') && getLocation(action).hostname === location.hostname) {
-        // Form action is HTTPS and is in the same domain :)
+    if (action.startsWith('https://')) {
+        // Form action is HTTPS
         ok = true;
     } else if (action.startsWith('http://')) {
         // Form action is HTTP :(
         ok = false;
     } else if (location.protocol === 'https:') {
-        // Form action is a relative path and the whole page is HTTPS :)
+        // Form action is a relative path and the whole page is HTTPS
         ok = true;
     } else {
         ok = false;
     }
     
-    $(this).css('background-color', ok ? '#ccffcc' : '#ffbbbb');
+    var pwClass;
+    if (!ok) {
+        pwClass = 'pwKO';
+    } else if (getLocation(action).hostname === location.hostname) {
+        pwClass = 'pwOK';
+    } else {
+        // Warn about cross domain in HTTPS
+        pwClass = 'pwWarn';
+    }
+    
+    $(this).addClass(pwClass);
     
     // Show form action on mouse hover.
     $(this).attr('title', 'form action: ' + action);
